1d3d915d3d6298bf56eae0748439955c7d6a3dff
ShyamCharanReddy <shyamcharan4444@gmail.com>
2026-03-18 20:57:14 +0530

    api_url

--- src/api/memes.js
+++ src/api/memes.js
@@ -3,7 +3,7 @@ export const getAllMemes = async() => {
     return await response.json();
 };
 
-const LOCAL_API_URL = 'http://localhost:5000/api';
+const LOCAL_API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";;
 
 export const registerUser = async (userData) => {
     const response = await fetch(`${LOCAL_API_URL}/users`, {
